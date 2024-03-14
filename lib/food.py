4d433f830b36a70d1c2c9ada77140ee8e9c155c0
KevinHarbour <kharbour22@gmail.com>
2024-03-14 16:34:03 -0400

    Met Deliverables

--- lib/food.py
+++ lib/food.py
@@ -1,2 +1,26 @@
+
 class Food:
-    pass
+    all = []
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        
+        Food.all.append(self)
+
+
+    @classmethod
+    def food_names(cls):
+        return [food.name for food in cls.all]
+
+    @classmethod
+    def average_price(cls):
+        list_of_prices = [food.price for food in cls.all]
+        return (sum(list_of_prices)/len(list_of_prices))
+    
+    @classmethod
+    def most_expensive(cls):
+        return (max(cls.all, key=lambda food: food.price))
+
+
+    
